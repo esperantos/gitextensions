5b2864bde0ae4560f80a8fe6e0f46dc284e25749
Sergei Matochkin <matochkin@skbkontur.ru>
2014-09-05 19:06:30 +0600

    added db schema
    Signed-off-by: freyberg <freyberg@skbkontur.ru>

---
+++ Review/database.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION update_review_time()
+RETURNS trigger AS
+$BODY$
+   BEGIN
+        NEW.update_time = now();
+RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE TABLE "public"."review_scheme" (
+	"author" varchar NOT NULL PRIMARY KEY, 
+	"reviewer" varchar
+)
+GO
+
+CREATE TABLE "public"."review" (
+	"commit_hash" varchar NOT NULL PRIMARY KEY, 
+	"status" integer, 
+	"comment" text, 
+	"change_author" varchar, 
+	"create_time" timestamp DEFAULT now(), 
+	"update_time" timestamp DEFAULT now()
+)
+GO
+CREATE TRIGGER "update_review"
+	AFTER UPDATE
+	ON review
+	FOR EACH ROW
+	EXECUTE PROCEDURE update_review_time();
+GO
